499c913b883e95aefa4c4a75e59bd750383f53bd
Kgnx7 <kgn@tutanota.com>
2020-04-10 00:01:37 +0300

    Updates

---
+++ precache-manifest.35a5d3f710ad76f497e6f8f0159bfec4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260fff80d26485c12c3d5a695c74c58b",
+    "url": "/bookifyru/index.html"
+  },
+  {
+    "revision": "a63605c12ec7ee7a460b",
+    "url": "/bookifyru/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "693c18798f3c30f6e170",
+    "url": "/bookifyru/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "a63605c12ec7ee7a460b",
+    "url": "/bookifyru/static/js/2.ad526d50.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/bookifyru/static/js/2.ad526d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693c18798f3c30f6e170",
+    "url": "/bookifyru/static/js/main.6b3edf34.chunk.js"
+  },
+  {
+    "revision": "020ec92cd3853d162e4b",
+    "url": "/bookifyru/static/js/runtime-main.102adbde.js"
+  },
+  {
+    "revision": "9ec24611442249d40d0e0f037542adb4",
+    "url": "/bookifyru/static/media/logo.9ec24611.svg"
+  },
+  {
+    "revision": "34ddb725a27cf155bf685e5b4b48db62",
+    "url": "/bookifyru/static/media/profile.34ddb725.svg"
+  }
+]);
